ec63ee75c8c679d73cd12f33a3e24c442fce05bb
Abhiram Ajith <abhiram.ajith@ucdconnect.ie>
2020-09-30 22:03:16 +0100

    Adding the C programs associated with the lab 1 quiz

---
+++ secondP.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("one\n");
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+    printf("two\n");
+    return 0;
+  }
