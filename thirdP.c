d9b5f686ef5eaa152da3b2fb776f5503e66b8f8b
Abhiram Ajith <abhiram.ajith@ucdconnect.ie>
2020-09-30 22:32:59 +0100

    Adding the C programs associated with the lab 1 quiz

---
+++ thirdP.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("========================\n");
+    printf("=                      =\n");
+    printf("= She sells sea shells =\n");
+    printf("=                      =\n");
+    printf("========================\n");
+
+    return 0;
+  }
