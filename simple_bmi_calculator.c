a262dcbc54c380d7a3f4bbac317dc1299a6d4ed4
Abhiram Ajith <abhiram.ajith@ucdconnect.ie>
2020-10-09 16:40:22 +0100

    simple bmi calculator

---
+++ simple_bmi_calculator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (void)
+{
+  int Height;
+  int Weight;
+
+  printf("Please enter your height in cm:\n");
+  scanf("%d", &Height );
+  printf("Your height is %d cm\n", Height);
+  return 0;
+
+
+
+
+
+}
