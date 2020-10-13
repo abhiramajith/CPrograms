b30723601d34759ec814e5de5eaf920718f6cd2b
Abhiram Ajith <abhiram.ajith@ucdconnect.ie>
2020-10-13 16:02:43 +0100

    A program that simulates a classic christmas song

--- partridge.c
+++ partridge.c
@@ -4,7 +4,7 @@ int main(void)
 {
   int day, i;
 
-  for(day = 1; day <= 4; day = day + 1)
+  for(day = 1; day <= 12; day = day + 1)
     {
       printf("On the %d day of Christmas, ", day);
       printf("my true love gave to me\n");
@@ -23,7 +23,22 @@ int main(void)
             printf("Three French hens,\n");
           else if(i == 4)
             printf("Four calling birds,\n");
-
+          else if(i == 5)
+            printf("Five golden rings,\n");
+          else if(i == 6)
+            printf("Six geese a laying,\n");
+          else if(i == 7)
+            printf("Seven swans a-swimming,\n");
+          else if(i == 8)
+            printf("Eight maids a-milking,\n");
+          else if(i == 9)
+            printf("Nine ladies dancing,\n");
+          else if(i == 10)
+            printf("Ten lords a-leaping,\n");
+          else if(i == 11)
+            printf("Eleven pipers piping,\n");
+          else if(i == 12)
+            printf("Twelve drummers drumming,\n");
     }
     printf("\n");
   }
