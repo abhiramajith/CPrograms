e8eebca0864f8d99e45884691eb1ada83ad704e6
Abhiram Ajith <abhiram.ajith@ucdconnect.ie>
2020-10-19 10:09:25 +0100

    A program that solves quadratic equation using the -b formula.

---
+++ quadratic.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+#include <math.h>
+ /*
+ Name: Abhiram Ajith - 20364441
+ Date: 19/10/2020
+ Title: A program that solves quadratic equations using the -b formula
+ Input: 3 variables a, b and c
+ Expected output: The number of real roots and the values of each root
+ */
+
+int main(void){
+
+double a ;
+double b ;
+double c ;
+
+
+printf("Please enter the value for coefficient a:");
+scanf("%lf", &a);
+printf("Please enter the value for coefficient b:");
+scanf("%lf", &b);
+printf("Please enter the value for coefficient c:");
+scanf("%lf", &c);
+
+double discriminant = (b*b)-(4*a*c);
+if (discriminant > 0) {
+  printf("The number of real roots is 2.");
+}
+else if (discriminant < 0){
+  printf("The number of real roots is 0.");
+}
+else if (discriminant == 0){
+  printf("The number of real roots is 1.");
+}
+
+
+return 0;
+
+
+
+}
