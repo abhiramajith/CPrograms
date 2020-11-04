c2a463d0ed0ff7bbe5af0b0fb496b0f179371106
Abhiram Ajith <abhiram.ajith@ucdconnect.ie>
2020-11-04 00:33:19 +0000

    A program that reads 4 numbers andputs them in descending order

--- orderMax.c
+++ orderMax.c
@@ -3,29 +3,42 @@
 int main(void) {
 
     double w, x, y, z;
-    double temp;
+    double temp;// A variable to store values w, x, y or z
     /* you may need some extra variables - but NO arrays please! */
 
     /* Do not change the following input statement */
     scanf("%lf%lf%lf%lf", &w,&x,&y,&z);
 
     /* Enter your solution here */
- if (w < x){
-   temp = w;
-   w = x;
-   x = temp;
+ if (w < x){  // To sort these numbers they must be swapped depending on their sizes
+   temp = w; // To ensure a value doesnt get overwritten it is stored in the variable temp
+   w = x;   // To swap the two numbers first you assign one to the other
+   x = temp;// Then the other variable is assigned the temp value.
  }
 
- if (x < y){
-   temp = x;
-   x = y
+ if (y < z){
+   temp = y;
+   y = z;
+   z = temp;
+ }
+
+ if (w < y){
+   temp = w;
+   w = y;
    y = temp;
  }
 
+if (x < z){
+  temp = x;
+  x = z;
+  z = temp;
+}
 
+if (x < y){
+  temp = x;
+  x = y;
+  y = temp;
+}
 
     /* Do not change the following output statement */
     printf("%.1f %.1f %.1f %.1f\n",w, x, y, z);
-
-    return 0;
-}
