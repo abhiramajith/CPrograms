fee6f035da8d292e89334df34f07268a47a32eca
Abhiram Ajith <abhiram.ajith@ucdconnect.ie>
2020-11-04 00:53:26 +0000

    A program that converts decimal to binary

--- decbin.c
+++ decbin.c
@@ -10,30 +10,37 @@
  */
 
 int main(void){
-
+  //Declaring variables
   int count, n, Num, digit, i;
   int a[size];
 
 
-  scanf("%d", &n);
+  scanf("%d", &n);// Reads user input for number they want to convert to binary
 
+//Assigning initial values
    digit = 0;
    Num = n;
-   count = -1;
+   count = -1;//computes length of array
+
 
    if (n == 0){
      printf("0 ");
    }
+
+/*This loop calculates each binary number that corrseponds to the user input
+  and adds it to the array, but it will need to be reversed */
 for(i = 0;Num > 0; i++){
 
-  digit = Num % 2;
-  a[i] = digit;
-  count ++ ;
-  Num = Num / 2;
+  digit = Num % 2;// gets the remainder of Num and assigns it to digit
+  a[i] = digit;// assigns the digit value (1 or 0) to the ith position on the array
+  count ++ ;// adds one to the counter
+  Num = Num / 2;// floor divides Num to get the next value to be calculated
 
 }
 
-printf("%d ", n);
+printf("%d ", n);//prints user input
+
+/*This loop uses the count variable to print the array in reverse order as required*/
 for (i = count ; i >= 0; i-- ){
   printf("%d", a[i]);
 
