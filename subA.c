53104f2ba89751511b92e8ba27ca60086ff6a91a
Abhiram Ajith <abhiram.ajith@ucdconnect.ie>
2020-11-13 20:45:15 +0000

    A program that reads in an array and prints the largest continous subarray

--- subA.c
+++ subA.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 #define MAX_SIZE 100
@@ -7,9 +6,11 @@ int main(void)
 {
   int a[MAX_SIZE];
   int N;
-  int i, j, k;
-  int x1, x2;
-  int sum, sumLargest, sumMax;
+  int i, j, k;//
+  int x1, x2;// These variables hold the values of i and j for printing out the correct sub array
+  int sum ;//holds the value of the largest continous sub array
+  int aLargest; //holds the value of the largest number in the array
+  int sumMax;//
 
   /* read in the array */
   scanf("%d", &N);
@@ -17,7 +18,15 @@ int main(void)
     scanf("%d", &a[i]);
 
   /* complete your program here ... */
+  /*
+  Name: Abhiram Ajith - 20364441
+  Date: 10/11/2020
+  Title: A program that reads in an array of size N, and computes the largest continous
+         subarray.
+  Expected output: Example [-4 7 2 1 9 -3] should return [7 2 1 9]
+  */
 
+//Edge case
   if (N<1){
     printf("Largest sum is 0 obtained from the subsequence [  ]\n");
     return -1;
@@ -25,14 +34,12 @@ int main(void)
   }
 
   sumMax = a[0];
-  sumLargest = a[0];
+  aLargest = a[0];
 
   for (i = 1;i < N; i++ ){
 
-    if (sumLargest<a[i]){
-      sumLargest = a[i];
-
-
+    if (aLargest<a[i]){
+      aLargest = a[i];
     }
   }
 
@@ -54,7 +61,7 @@ int main(void)
     }
   }
 
-if ( sumMax > sumLargest){
+if ( sumMax > aLargest){
 printf("\nLargest sum is %d obtained from the subsequence [", sumMax);
 for (k=x1;k<=x2;k++){
   printf(" %d", a[k]);
