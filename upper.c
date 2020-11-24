37874341dbefec9daceb477a3de77edbe03a5c3e
Abhiram Ajith <abhiram.ajith@ucdconnect.ie>
2020-11-24 19:18:56 +0000

    A program that converts a lowercase string to uppercase

---
+++ upper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define MAX_MSG_SIZE 256
+void capitalise(char str[]);
+int main(void)
+{
+  char msg[MAX_MSG_SIZE+1];
+  /* the fgets function is used to read a string from the console */
+  fgets(msg, MAX_MSG_SIZE, stdin);
+
+  /* complete your answer here */
+  int i;
+ for (i=0;i<MAX_MSG_SIZE; i++){
+   if (msg[i] >= 'a' && msg[i]<= 'z' && msg[i]!='\0'){
+     msg[i]= msg[i] - 32;
+   }
+ }
+ printf("%s", msg);
+  return 0;
+}
