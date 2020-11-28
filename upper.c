d4ceabd650f9da6d54034d47844f9e6c2fc9fe9e
abhiramajith <abhiram.ajith@ucdconnect.ie>
2020-11-28 18:41:37 +0000

    A program that converts a string with lowercase letters into a string with all uppercase letters

--- upper.c
+++ upper.c
@@ -9,10 +9,19 @@ int main(void)
   fgets(msg, MAX_MSG_SIZE, stdin);
 
   /* complete your answer here */
-  int i;
+  /*
+  Name: Abhiram Ajith - 20364441
+  Date: 24/11/2020
+  Title: A program that converts a string containing lowercase letters to uppercase
+  Expected output: hello world! should return HELLO WORLD! 
+  */
+  int i;//loop iterator
+
+  //loops while the string value for i is not equal to the terminator
  for (i=0; msg[i]!= '\0'; i++){
-   if (msg[i] >= 'a' && msg[i]<= 'z'){
-     msg[i]+= 'A'-'a';
+   if (msg[i] >= 'a' && msg[i]<= 'z'){//if the i index of the string is greater than the ascii value of 'a' and less than 'z'
+                                      //i.e if the ith index is a lowercase letter
+     msg[i]+= 'A'-'a';// adds -32 to the value of msg[i] making it uppercase as any lowercase letter -32 is its respective uppercase in ascii
    }
  }
  printf("%s", msg);
