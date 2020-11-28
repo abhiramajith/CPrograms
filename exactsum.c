603a5700b86e10214f04db715e2fed43142a8bc0
abhiramajith <abhiram.ajith@ucdconnect.ie>
2020-11-28 19:00:01 +0000

    A program that finds from amongst a set of positive lengths in nanometres, a pair of lengths that sum exactly to a target length given in centimetres.

--- exactsum.c
+++ exactsum.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+/*
+Name: Abhiram Ajith - 20364441
+Date: 24/11/2020
+Title: A program that finds from amongst a set of positive lengths in nanometres, a pair of
+       lengths that sum exactly to a target length that is given in centimetres.
+Expected output: Target length = 15
+                 Found: 70214190 + 79785810 == 150000000
+*/
+
 
 int main(void)
 {
@@ -57,46 +66,26 @@ int main(void)
   /* Now all the input data has been read in
      search for the required pair of lengths... */
      int Nano_x;
-     Nano_x= x*10000000;
-     i = 0;
-     j = n-1;
+     Nano_x= x*10000000;// as the target length is given in centimetres it must be converted to nanometers
+     i = 0; // start of the array
+     j = n-1;// end of the array
 
+//loops while i is less than j
      while (i < j){
-       if ((lengths[i] + lengths[j])==Nano_x){
+       if ((lengths[i] + lengths[j])==Nano_x){//pair is found
          printf("Found: %d + %d == %d\n", lengths[i], lengths[j], lengths[i]+lengths[j] );
          return -1;
        }
 
-       if ((lengths[i] + lengths[j]) < Nano_x){
+       if ((lengths[i] + lengths[j]) < Nano_x){//if total is less than sum increment i
          i++;
        }
-       else{
+       else{//if total is more than sum decrement j
          j--;
        }
      }
 
-     printf("No matching pair found\n");
-
-
-
-
-     /*
-     for (i=0;i<n;i++){
-         for (j=n-1;j>=0;j--){
-           if ((lengths[i] + lengths[j])==Nano_x) {
-             printf("Found: %d + %d == %d\n", lengths[i], lengths[j], lengths[i]+lengths[j] );
-             found = true;
-             return -1;
-           }
-
-         }
-
-       }
-
-      if (!found){
-        printf("No matching pair found\n");
-       }
-       */
+     printf("No matching pair found\n");//if no pair is found
 
 
 
