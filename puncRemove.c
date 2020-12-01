f155221eb78089c452bc0cf71229c9d668b88e00
abhiramajith <abhiram.ajith@ucdconnect.ie>
2020-12-01 20:00:15 +0000

    A program that removes punctuation in a given sentence.

--- puncRemove.c
+++ puncRemove.c
@@ -8,14 +8,21 @@ int main(void)
   fgets(msg, MAX_MSG_LEN, stdin);
 
   /* write your solution here ... */
-  int i = 0;
-  int j;
+  /*
+  Name: Abhiram Ajith - 20364441
+  Date: 01/12/2020
+  Title:  A program that removes punctuation in a given sentence.
+  Expected output: 	'Too, many, punctuation marks:; by far!!' should return 'Too many punctuation marks by far'
+  */
+  int i = 0;//used to iterate through msg[]
+  int j;//used to iterate through msg[]
+
 while (msg[i]!= '\0'){
-  if (msg[i] == '!' || msg[i] == '`' ||  msg[i] == '?' ||  msg[i] == ';' || msg[i] == ':'||  msg[i] == ',' ||  msg[i] == '.'    ){
-    for (j=i; msg[j]!= '\0';j++){
-        msg[j]= msg[j+1];
+  if (msg[i] == '!' || msg[i] == '`' ||  msg[i] == '?' ||  msg[i] == ';' || msg[i] == ':'||  msg[i] == ',' ||  msg[i] == '.'    ){//there are 7 different punctuation symbols
+    for (j=i; msg[j]!= '\0';j++){//loops from msg[i] to the end of msg[]
+        msg[j]= msg[j+1];//assigns msg[j] to msg[j+1] overwriting the punctuation marke and shifting evrything to the left once
     }
-    i--;
+    i--;// decrements i if there is a punctuation in msg[i] in case there is more than one punctuation in a row
   }
   i++;
 }
