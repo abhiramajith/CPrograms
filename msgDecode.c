c335cfef46f7dc698a88181477717979aa6c2f6e
abhiramajith <abhiram.ajith@ucdconnect.ie>
2020-12-01 19:40:05 +0000

    A program that decodes a message that has been encoded using a permutation array of the alphabet

--- msgDecode.c
+++ msgDecode.c
@@ -12,28 +12,36 @@ int main(void)
   fgets(msg, MAX_STR_LEN, stdin);
 
   /* write your answer here */
-  int i=0;
-  int j;
-  int x;
-  while (msg[i]!= '\0'){
-    if (msg[i] >= 'a' && msg[i]<= 'z'){
-      for (j=0;perm[j]!= '\0'; j++){
+  /*
+  Name: Abhiram Ajith - 20364441
+  Date: 01/12/2020
+  Title:  A program that decodes a message that has been encoded using a permutation array of the alphabet.
+  Expected output: 'Bicyik fibbqai' should 'Secret message'
+  */
+
+  int i=0;//used to iterate through msg[]
+  int j;//used to iterate through perm[]
+  int x;//holds the index of the respective letter in perm[]
+
+  while (msg[i]!= '\0'){//loops for the length of msg[]
+    if (msg[i] >= 'a' && msg[i]<= 'z'){//Condition for lowercase
+      for (j=0;perm[j]!= '\0'; j++){//loops through perm[] until msg[i]=perm[j], this gives the index of the letter in msg[i] in perm[]
         if (perm[j]==msg[i]){
           x=j;
         }
       }
-      msg[i]= 'a'+ x;
+      msg[i]= 'a'+ x;//adds x to 'a' which will give the corresponding letter in the alphabet in lowercase and assign it to msg[i]    
 
     }
 
-    else if (msg[i] >= 'A' && msg[i]<= 'Z'){
-      msg[i]+= 'a'-'A';
+    else if (msg[i] >= 'A' && msg[i]<= 'Z'){//Condition for uppercase
+      msg[i]+= 'a'-'A';//converts msg[i] to lowercase so it can be found in perm[]
       for (j=0;perm[j]!= '\0'; j++){
         if (perm[j]==msg[i]){
           x=j;
         }
       }
-  msg[i]= 'A'+ x;
+  msg[i]= 'A'+ x;//adds x to 'A' which will give the corresponding letter in the alphabet in uppercase and assign it to msg[i]
     }
     i++;
   }
