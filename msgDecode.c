cb260df70c0ce622f224e93bbeda22d380a98a58
abhiramajith <abhiram.ajith@ucdconnect.ie>
2020-12-01 19:09:19 +0000

    A program that decodes a message that has been encoded using a permutation array of the alphabet

---
+++ msgDecode.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define MAX_STR_LEN 256
+int main(void)
+{
+  /* keep the perm[] array fixed as it is here, though
+     your program should work for any permutation array */
+  char perm[] = "qjczieaungsdfxmphybklortvw";
+
+  char msg[MAX_STR_LEN+1];
+
+  /* read the message from the terminal using fgets */
+  fgets(msg, MAX_STR_LEN, stdin);
+
+  /* write your answer here */
+  int i=0;
+  int x ;
+  while (msg[i]!= '\0'){
+    if (msg[i] >= 'a' && msg[i]<= 'z'){
+    x=  msg[i] - 'a';
+    msg[i]=perm[x];
+    }
+
+    else if (msg[i] >= 'A' && msg[i]<= 'Z'){
+    x=  msg[i] - 'A';
+    msg[i]=perm[x] + 'A'-'a';
+    }
+    i++;
+  }
+
+
+printf("%s", msg);
+  return 0;
+}
